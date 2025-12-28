1fb45eaeee43543deb6525de27edb454b96268f6
Filip Wośko <filipwosko96@gmail.com>
2025-12-28 18:09:23 +0100

    poprawa message, zeby wysylal JSON-a

--- src/main/java/com/example/pictures_annotator/presentation/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/pictures_annotator/presentation/handler/GlobalExceptionHandler.java
@@ -9,26 +9,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Collections;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(PictureNotFoundException.class)
-    public ResponseEntity<String> handlePictureNotFound(PictureNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ExceptionHandler(PictureNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlePictureNotValid(PictureNotValidException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonMap("message", ex.getMessage()));
     }
 
-    @ExceptionHandler(PictureNotValidException.class)
-    public ResponseEntity<String> handleBoundingBoxNotFound(PictureNotValidException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+
+    @ExceptionHandler(PictureNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePictureNotFound(PictureNotFoundException ex) {
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(Collections.singletonMap("message", ex.getMessage()));
     }
 
     @ExceptionHandler(BoundingBoxNotFoundException.class)
-    public ResponseEntity<String> handleBoundingBoxNotFound(BoundingBoxNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleBoundingBoxNotFound(BoundingBoxNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonMap("message", ex.getMessage()));
     }
 
     @ExceptionHandler(BoundingBoxOutOfImageException.class)
-    public ResponseEntity<String> handleBoundingBoxOutOfImage(BoundingBoxOutOfImageException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleBoundingBoxOutOfImage(BoundingBoxOutOfImageException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Collections.singletonMap("message", ex.getMessage()));
     }
 }
